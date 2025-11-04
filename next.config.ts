291dbe05a9b10ee48c61f87dd8e4e612726f0319
Paul <21148863+pruales@users.noreply.github.com>
2025-11-03 18:06:19 -0800

    fixing type errors

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import { withSentryConfig } from "@sentry/nextjs";
-import type NextConfig from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
